5bf07e8d83b6b6892e0abbba4f7d5a7e1791bc50
Tejas Dadhaniya <tejasdadhaniya2512@gmail.com>
2023-09-21 18:27:26 +0530

    db dir

---
+++ db/mongoops.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+
+class MongoOps:
+     def __init__(self) -> None:
+         mongo_pwd = "zfpvdQzVsLcRcgvM"
+         uri = f"mongodb+srv://waferQuality:{mongo_pwd}@cluster0.va0if0g.mongodb.net/?retryWrites=true&w=majority"
+     
+     def connect():
+         pass
+     
+     def create(database=True):
+         pass
+     
+     def update(): 
+         pass
+     
+     def add(one=True):
+         pass
+     
+     def remove(one=True):
+         pass
+     
