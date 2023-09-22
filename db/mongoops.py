b3abc32686dc214763fb4c1a547a5a337bcb4658
Tejas Dadhaniya <tejasdadhaniya2512@gmail.com>
2023-09-22 15:54:32 +0530

    updated code in db/

--- db/mongoops.py
+++ db/mongoops.py
@@ -1,23 +1,27 @@
 from pymongo import MongoClient
+import json
 
 
 class MongoOps:
-     def __init__(self):
-         mongo_pwd = "zfpvdQzVsLcRcgvM"
-         uri = f"mongodb+srv://waferQuality:{mongo_pwd}@cluster0.va0if0g.mongodb.net/?retryWrites=true&w=majority"
-         client = MongoClient(uri)
-         db = client["waferquality"]
-         self.col = db["sensors"]
-     
-     def create(self, database=True):
-         pass
-     
-     def update(): 
-         pass
-     
-     def add(self, documents:list):
-         self.col.insert_many(documents)
-              
-     def remove(self, one=True):
-         pass
-     
+    def __init__(self):
+        with open("cred.json") as file:
+            cred = json.loads(file.read())
+        uri = cred['uri']
+        client = MongoClient(uri)
+        db = client["waferquality"]
+        self.col = db["sensors"]
+
+    def create(self, database=True):
+        pass
+
+    def update(self):
+        pass
+
+    def add(self, documents: list):
+        self.col.insert_many(documents)
+
+    def remove(self, one=True):
+        pass
+
+
+obj = MongoOps()
--- db/qualitychecks.py
+++ db/qualitychecks.py
@@ -3,7 +3,6 @@ import json
 import os
 from mongoops import MongoOps
 
-
 # Define file paths
 schema_file = "schema_training.json"
 data_dir = "Training_Batch_Files"
@@ -13,12 +12,14 @@ db = MongoOps()
 with open(schema_file) as file:
     checks = json.load(file)
 
+
 # Function to check the file name format
 def name_check(name):
     parts = name.split('_')
     return (len(parts) == 3 and
             len(parts[1]) == checks['LengthOfDateStampInFile'] and
             len(parts[2].split('.')[0]) == checks['LengthOfTimeStampInFile'])
+
 
 # Process each file in the data directory
 for filename in os.listdir(data_dir):
