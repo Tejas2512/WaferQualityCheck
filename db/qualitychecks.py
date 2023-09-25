7824ae14685a3d0055023a1437acde703d4835cf
Tejas Dadhaniya <tejasdadhaniya2512@gmail.com>
2023-09-25 16:57:32 +0530

    updated testcase and covert db to module

--- db/qualitychecks.py
+++ db/qualitychecks.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import json
 import os
-from mongoops import MongoOps  # Assuming you have a separate module for MongoOps
+from WaferQualityCheck.db.mongoops import MongoOps  # Assuming you have a separate module for MongoOps
+import shutil
+
 
 # Define file paths
-SCHEMA_FILE = "schema_training.json"
-DATA_DIR = "Training_Batch_Files"
+SCHEMA_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "schema_training.json"))
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "Training_Batch_Files"))
+INVALID_FILES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "Invalid_Data"))
 
 
 def load_schema_checks(schema_file):
@@ -36,16 +39,22 @@ def process_file(filename, checks, db):
                 db.add(documents)
             except ValueError as e:
                 print(f"Data type conversion error in {filename}: {e}")
+                shutil.move(src=os.path.join(DATA_DIR, filename), dst=INVALID_FILES_DIR)
         else:
             print(f"Column count mismatch in {filename}")
+            shutil.move(src=os.path.join(DATA_DIR, filename), dst=INVALID_FILES_DIR)
     else:
         print(f"Invalid file name format in {filename}")
+        shutil.move(src=os.path.join(DATA_DIR, filename), dst=INVALID_FILES_DIR)
 
 
-def main():
-    """Main function to process files."""
+def main(credentials="default"):
+    """
+        Main function to process files.
+        :param credentials: The name of the credentials to use from the 'cred.json' file.
+    """
     checks = load_schema_checks(SCHEMA_FILE)
-    db = MongoOps()
+    db = MongoOps(credentials=credentials)
 
     for filename in os.listdir(DATA_DIR):
         process_file(filename, checks, db)
--- test_module.py
+++ test_module.py
@@ -1,29 +1,33 @@
+import os
 import unittest
-from db.mongoops import MongoOps
+import shutil
+from WaferQualityCheck.db.mongoops import MongoOps
+from WaferQualityCheck.db import qualitychecks
 
 
 class TestMongoOps(unittest.TestCase):
+
     def setUp(self):
-        # Create an instance of MongoOps for testing with 'test' credentials
+        """
+        Set up the test case by creating an instance of MongoOps for testing with 'test' credentials.
+        """
         self.mongo_obj = MongoOps(credentials='test')
 
-    def test_insert_data(self):
+    def test_db1_insert_data(self):
         """
         Test inserting data into the collection and check if the count matches.
         """
-        # Prepare sample data
-        documents = [{"name": "John"}, {"name": "Alice"}]
-
-        # Insert data into the collection
-        self.mongo_obj.add(documents)
+        # Set the data directory for quality checks
+        qualitychecks.DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "db", "Unittest_Batch_Files"))
+        qualitychecks.main(credentials="test")
 
         # Count the documents in the collection
         count = self.mongo_obj.col.count_documents({})
 
         # Assert that the count matches the expected value
-        self.assertEqual(count, 2)
+        self.assertEqual(count, 5)
 
-    def test_delete_collection(self):
+    def test_db2_delete_collection(self):
         """
         Test deleting the collection and check if it's not in the list of collections.
         """
@@ -36,7 +40,7 @@ class TestMongoOps(unittest.TestCase):
         # Assert that the collection name is not in the list of collections
         self.assertNotIn(self.mongo_obj.col.name, collections)
 
-    def test_delete_database(self):
+    def test_db3_delete_database(self):
         """
         Test deleting the database and check if it's not in the list of databases.
         """
@@ -49,6 +53,20 @@ class TestMongoOps(unittest.TestCase):
         # Assert that the database name is not in the list of databases
         self.assertNotIn(self.mongo_obj.db.name, databases)
 
+    def test_db4_revert_data(self):
+        """
+        Test reverting data files.
+        """
+        FILES = ["Wafer_31022023_000000.csv", "Wafer_31022023_111.csv"]
+        UNITTEST_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "db", "Unittest_Batch_Files"))
+        for file in FILES:
+            shutil.move(src=os.path.join(qualitychecks.INVALID_FILES_DIR, file),
+                        dst=UNITTEST_DATA_DIR)
+
+        # Check if the number of files in the test directory matches the expected count
+        self.assertEqual(len(os.listdir(UNITTEST_DATA_DIR)), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
+
